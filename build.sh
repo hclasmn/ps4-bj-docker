3aecc7fa84f2a78a0304f20f6a2bd6d24e0ae4dc
hclasmn <32213103+hclasmn@users.noreply.github.com>
2020-09-02 17:25:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 !/bin/bash
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuilder
-docker buildx build --tag scjtqs/miraigo:latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386 --push . \
+docker buildx build --tag hclasmn/ps4-bj-docker:latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386 --push . \
 && docker buildx rm mybuilder
