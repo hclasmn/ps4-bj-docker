82c2d4992467e00a0dc2fb45eb7dc044647ce218
hclasmn <32213103+hclasmn@users.noreply.github.com>
2020-09-02 17:42:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuilder
 docker buildx build --tag hclasmn/ps4-bj-docker:latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386 --push . \
